1b076f095da5991e81165b70efec239561cf5393
Reinaldo Duque <88598441+reinaldodu@users.noreply.github.com>
2025-11-21 14:13:35 +0000

    Actualización del CRUD y comentarios en app.py

--- app.py
+++ app.py
@@ -19,20 +19,24 @@ productos = {
     6: {"id": 6, "nombre": "Webcam", "precio": 200.0},
 }
 
+# Ruta raíz
 @app.get("/")
 def root():
     return {"message": "API FastAPI en Docker funcionando"}
 
+# Listar los productos
 @app.get("/productos")
 def get_productos():
     return list(productos.values())
 
+# Listar un producto
 @app.get("/productos/{producto_id}")
 def get_producto(producto_id: int):
     if producto_id not in productos:
         raise HTTPException(status_code=404, detail="Producto no encontrado")
     return productos[producto_id]
 
+# Crear un producto
 @app.post("/productos")
 def create_producto(producto: Producto):
     if producto.id in productos:
@@ -40,6 +44,19 @@ def create_producto(producto: Producto):
     productos[producto.id] = producto.dict()
     return {"message": "Producto creado", "item": producto}
 
+# Actualizar un producto
+@app.put("/productos/{producto_id}")
+def update_producto(producto_id: int, producto: Producto):
+    if producto_id not in productos:
+        raise HTTPException(status_code=404, detail="Producto no encontrado")
+
+    if producto_id != producto.id:
+        raise HTTPException(status_code=400, detail="El ID del producto no coincide con la URL")
+
+    productos[producto_id] = producto.dict()
+    return {"message": "Producto actualizado", "item": producto}
+
+# Eliminar un producto
 @app.delete("/productos/{producto_id}")
 def delete_producto(producto_id: int):
     if producto_id not in productos:
